44e5decaa8a5c1291c097b538b0b526d91cdb95d
jun-ki <75047374+clarityaqua@users.noreply.github.com>
2026-06-09 17:26:35 +0900

    add flask project

---
+++ main.py
@@ -0,0 +1,40 @@
+# BLUEPRINT | DONT EDIT
+
+from flask import Flask, render_template, request
+import json
+
+app = Flask("JobScraper")
+
+
+def load_jobs():
+    with open("jobs.json", "r") as f:
+        return json.load(f)
+
+# /BLUEPRINT
+
+
+# 👇🏻 YOUR CODE 👇🏻:
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/search")
+def search():
+    jobs = load_jobs()
+    keyword = request.args.get("keyword")
+    results = []
+    for job in jobs:
+        if keyword.lower() in job["title"].lower():
+            results.append(job)
+    return render_template("search.html", results=results, keyword=keyword)
+
+# /YOUR CODE
+
+
+# BLUEPRINT | DONT EDIT
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# /BLUEPRINT
